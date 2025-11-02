42b873642029ba795ba0922b5fbd00afac4ddc1f
Azhar Farooq <azharfarooq750@gmail.com>
2025-11-03 01:27:58 +0530

    Fixed dotenv setup

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV != "production") {
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: "process.env.SECRET",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -56,7 +56,7 @@ store.on("error", () => {
 
 const sessionOptions = {
   store,
-  secret: "process.env.SECRET",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
